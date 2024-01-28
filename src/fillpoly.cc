e9f25a7869706fa5964ac42fe5f0fdd41cf131d7
pfzoz <pedrozoz.sizaan@gmail.com>
2024-01-28 13:40:16 -0300

    better-veditor

--- src/fillpoly.cc
+++ src/fillpoly.cc
@@ -27,8 +27,8 @@ CEditor ceditor;
 VEditor veditor;
 PolygonList *polygon_list;
 DialogBox *thickness_control;
-ColorWheel *color_wheel, *bg_color_wheel;
-LightBar *light_bar, *bg_light_bar;
+ColorWheel *color_wheel, *bg_color_wheel, *veditor_color_wheel;
+LightBar *light_bar, *bg_light_bar, *veditor_lightbar;
 Canvas *canvas;
 
 void load_assets()
@@ -79,10 +79,18 @@ void resize(int width, int height)
         int actual_x, actual_y;
         canvas->get_vertex_pos(veditor.vertex, &actual_x, &actual_y);
         veditor.update_geometry(actual_x - 9 * cvw, actual_y - 10 * cvh, 18 * cvw, 10 * cvh);
+        veditor_color_wheel->set_radius(veditor.geometry.w / 4);
+        veditor_color_wheel->set_position(veditor.geometry.x + (veditor.geometry.w / 2) - veditor_color_wheel->geometry.w / 2, veditor.geometry.y - veditor_color_wheel->geometry.h);
+        veditor_lightbar->set_dimensions(2 * cvw, veditor_color_wheel->geometry.h);
+        veditor_lightbar->set_position(veditor_color_wheel->geometry.x + veditor_color_wheel->geometry.w, veditor_color_wheel->geometry.y);
     }
     else
     {
         veditor.update_dimensions(18 * cvw, 10 * cvh);
+        veditor_color_wheel->set_radius(veditor.geometry.w / 4);
+        veditor_color_wheel->set_position(veditor.geometry.x + (veditor.geometry.w / 2) - veditor_color_wheel->geometry.w / 2, veditor.geometry.y - veditor_color_wheel->geometry.h);
+        veditor_lightbar->set_dimensions(2 * cvw, veditor_color_wheel->geometry.h);
+        veditor_lightbar->set_position(veditor_color_wheel->geometry.x + veditor_color_wheel->geometry.w, veditor_color_wheel->geometry.y);
     }
 }
 
@@ -104,6 +112,15 @@ void bg_lightbar_onclick(int x, int y, bool hit)
     }
 }
 
+void veditor_lightbar_onclick(int x, int y, bool hit)
+{
+    if (hit)
+    {
+        veditor_lightbar->set_luminance(x, y);
+        veditor_color_wheel->set_luminance((float)veditor_lightbar->get_luminance()/255);
+    }
+}
+
 void canvas_onclick(int x, int y, bool hit)
 {
     if (hit && !veditor.is_selected)
@@ -119,7 +136,11 @@ void canvas_onclick(int x, int y, bool hit)
             canvas->draw_polygon(x, y, color);
     }
     else if (hit && veditor.is_selected)
+    {
         veditor.unselect();
+        veditor_color_wheel->set_visible(false);
+        veditor_lightbar->set_visible(false);
+    }
 }
 
 void canvas_onright_click(int x, int y, bool hit)
@@ -143,16 +164,28 @@ void canvas_onmiddle_click(int x, int y, bool hit)
             canvas->get_vertex_pos(selected, &screen_x, &screen_y);
             veditor.select_vertex(selected);
             veditor.update_position(screen_x - veditor.geometry.w / 2, screen_y - selected->thickness - veditor.geometry.h);
+            veditor_color_wheel->set_position(veditor.geometry.x + (veditor.geometry.w / 2) - veditor_color_wheel->geometry.w / 2, veditor.geometry.y - veditor_color_wheel->geometry.h);
+            veditor_lightbar->set_position(veditor_color_wheel->geometry.x + veditor_color_wheel->geometry.w, veditor_color_wheel->geometry.y);
+            veditor_color_wheel->set_visible(true);
+            veditor_lightbar->set_visible(true);
         }
         else
+        {
             veditor.unselect();
+            veditor_color_wheel->set_visible(false);
+            veditor_lightbar->set_visible(false);
+        }
     }
 }
 
 void handle_miss_click(int x, int y, bool hit)
 {
     if (!hit && veditor.is_selected)
+    {
         veditor.unselect();
+        veditor_color_wheel->set_visible(false);
+        veditor_lightbar->set_visible(false);
+    }
 }
 
 void handle_colorwheel_click(int x, int y, bool hit)
@@ -178,7 +211,11 @@ void handle_reset_onlick(int x, int y, bool hit)
         polygon_list->clear();
         message_text->update_text("Clique com o botão esquerdo para começar a desenhar um polígono.");
         if (veditor.is_selected)
+        {
             veditor.unselect();
+            veditor_color_wheel->set_visible(false);
+            veditor_lightbar->set_visible(false);
+        }
     }
 }
 
@@ -324,6 +361,25 @@ void handle_on_bg_color_click(int x, int y, bool hit)
     }
 }
 
+void handle_on_v_color_click(int x, int y, bool hit)
+{
+    int red = -1, blue = -1, green = -1;
+    if (hit)
+    {
+        veditor_color_wheel->get_color(x, y, &red, &green, &blue);
+        if (red != -1 && blue != -1 && green != -1)
+        {
+            veditor.red->update_text(std::to_string(red));
+            veditor.green->update_text(std::to_string(green));
+            veditor.blue->update_text(std::to_string(blue));
+            veditor.update_red();
+            veditor.update_green();
+            veditor.update_blue();
+            canvas->set_colors();
+        }
+    }
+}
+
 void polygon_list_onclick(int x, int y, bool hit)
 {
     if (hit)
@@ -333,7 +389,11 @@ void polygon_list_onclick(int x, int y, bool hit)
         {
             canvas->delete_polygon(polygon);
             if (veditor.is_selected)
+            {
                 veditor.unselect();
+                veditor_color_wheel->set_visible(false);
+                veditor_lightbar->set_visible(false);
+            }
         }
     }
 }
@@ -351,6 +411,7 @@ int main(int argc, char *args[])
         printf("Não pode inicializar as fontes do SDL. Encerrando...\n");
         std::exit(1);
     }
+    
     /* Create Scene */
     printf("Creating scene... ");
     main_window = SDL_CreateWindow("Fillpoly", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_RESIZABLE | SDL_WINDOW_ALLOW_HIGHDPI);
@@ -371,10 +432,14 @@ int main(int argc, char *args[])
     veditor = VEditor((30 * VW) / 2 - veditor.geometry.w / 2, 90 * cvw - 10 * VH, 18 * VW, 10 * VH, default_font);
     color_wheel = new ColorWheel(ceditor.geometry.x + (ceditor.geometry.w / 2) - (5 * VW) / 2, ceditor.geometry.y - 10 * VW, 5 * VW);
     bg_color_wheel = new ColorWheel(color_wheel->geometry.w, ceditor.geometry.h, 5 * VW);
+    veditor_color_wheel = new ColorWheel(veditor.geometry.w, veditor.geometry.w, veditor.geometry.w / 4);
+    veditor_color_wheel->set_visible(false);
     bg_color_wheel->set_visible(false);
     light_bar = new LightBar(2 * VW, color_wheel->geometry.h);
     bg_light_bar = new LightBar(2 * VW, color_wheel->geometry.h);
     bg_light_bar->set_visible(false);
+    veditor_lightbar = new LightBar(2 * VW, veditor_color_wheel->geometry.h);
+    veditor_lightbar->set_visible(false);
     reset_btn = new Button("Limpar", 10 * VW, 5 * VH, default_font);
     background_btn = new Button("Mudar Cor de Fundo", 20 * VW, 5 * VH, default_font);
     polygon_list = new PolygonList(10 * cvw);
@@ -388,6 +453,9 @@ int main(int argc, char *args[])
     veditor.red->z_index = 1;
     veditor.green->z_index = 1;
     veditor.blue->z_index = 1;
+    //
+    veditor_color_wheel->z_index = 1;
+    veditor_lightbar->z_index = 1;
     // Canvas
     canvas->set_onclick(canvas_onclick);
     canvas->set_onright_click(canvas_onright_click);
@@ -407,6 +475,8 @@ int main(int argc, char *args[])
     light_bar->set_onclick(lightbar_onclick);
     bg_light_bar->set_onclick(bg_lightbar_onclick);
     polygon_list->set_onclick(polygon_list_onclick);
+    veditor_color_wheel->set_onclick(handle_on_v_color_click);
+    veditor_lightbar->set_onclick(veditor_lightbar_onclick);
     /* Add Components */
     main_scene->add_component(canvas);
     main_scene->add_component(message_text);
@@ -423,6 +493,8 @@ int main(int argc, char *args[])
     main_scene->add_component(bg_light_bar);
     main_scene->add_component(polygon_list);
     main_scene->add_component(delete_message);
+    main_scene->add_component(veditor_color_wheel);
+    main_scene->add_component(veditor_lightbar);
     main_scene->onresize(resize);
     /* Main Loop */
     while (!main_scene->quit)
